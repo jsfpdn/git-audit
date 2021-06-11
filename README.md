6ff8efac704d0597bfb8c6451455dfa47d4c7e71
Josef Podany <josef.podany.ml@gmail.com>
2021-06-11 17:52:50 +0200

    refac: use Cobra to manage CLI commands

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # git-audit-tool
+
 Git Audit tool computing changelogs from public GitHub repositories. 
+
+## Install
+
+```bash
+go get github.com/jsfpdn/git-audit
+```
+
+## Usage
+
+### Using CLI Client
+
+```bash
+git-audit client --owner <OWNER> --repository <REPOSITORY>
+```
+
+### Running the gRPC Server
+
+```bash
+git-audit server --port <PORT>
+```
 
 
 ## TODOs
---
+++ cmd/root.go
@@ -0,0 +1,55 @@
+// Package cmd providing necessary commands.
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "git-audit",
+	Short: "Git Audit generates changelogs from public GitHub repositories",
+	Long: `Git Audit generates changelogs from public GitHub repositories.
+It can be run either as a gRPC server or a CLI client.`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	cobra.CheckErr(rootCmd.Execute())
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".git-audit" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".git-audit")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
